16eeac55d2495976b9dec02ead9cf20b47d6c5c7
cdiaz19 <cristian.diaz.jim@gmail.com>
2017-04-11 00:20:29 -0600

    inicio de vector del puente

--- proyecto0.c
+++ proyecto0.c
@@ -9,10 +9,11 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-#define numCarsOE 6
-#define numCarsEO 6
+#define numCarsOE 12
+#define numCarsEO 10
+#define NumVector 8
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t mutexes[NumVector];
 
 /* Estructuras */
 typedef struct StructDriver {
@@ -20,24 +21,40 @@ typedef struct StructDriver {
     int direction;
 } QStructDriver;
 
+typedef struct StructBridge {
+    int size;
+} QStructBridge;
+
 /* Metodos */
 int randomType(){
-  return (rand() % 2); // Asigna los random 0 entre 1.
+  return (rand() % 2);
 }
 
 void *createDiverThread(QStructDriver *QSEst) {
 
-  QSEst->type = randomType(); // Asigna los random del tipo de carro,[ 0 -> Normal / 1 -> Ambulancia ]
+  QSEst->type = randomType(); 
   QSEst->direction = 1;
   printf( "Tipo: %i - Direccion: %i. \n", QSEst->type, QSEst->direction );
 }
 
+void bridgeMutex(int arra1[]) {
+  //Crea el vector de Mutex
+  printf( "\nVector de Mutex. \n" );
+  for (int i = 0; i < NumVector; i++) {
+    pthread_mutex_init(&mutexes[i], NULL);
+  }
+
+  for (int i=0; i < numCarsOE; i++) {
+    printf("%i ", arra1[i] );
+  }
+}
+
 int main(void) {
   srand(time(NULL));
+  QStructDriver QSEst;
   pthread_t thread;
   int arrayDriverOE[numCarsOE];
-  int arrayDriverEO[numCarsEO]; 
-  QStructDriver QSEst;
+  int arrayDriverEO[numCarsEO];
 
   //Realiza hilos de Carros Oeste - Este
   printf( "Se Crean los %i Carros de [ O-E ]. \n", numCarsOE );
@@ -50,9 +67,11 @@ int main(void) {
   //Realiza hilos de Carros Oeste - Este
   printf( "\nSe Crean los %i Carros de [ E-O ]. \n", numCarsEO );
   for ( int i = 0 ; i < numCarsEO ; i ++ ) {
-    arrayDriverEO[i] = pthread_create ( &thread, NULL, createDiverThread, (void*) &QSEst ) ;
+    arrayDriverEO[i] = pthread_create ( &thread, NULL, createDiverThread, (void*) &QSEst );
     pthread_join ( thread, NULL );
   }
+
+  bridgeMutex(arrayDriverOE);
 
   return 0;
 }
